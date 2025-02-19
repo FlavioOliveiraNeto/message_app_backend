f01bf9052f42c62d875b294b6f07651b0d89f241
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2025-02-19 11:28:41 -0300

    Adição de validação nas mensagens

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,14 @@ class Message < ApplicationRecord
     belongs_to :receiver, class_name: 'User'
 
     has_one_attached :attachment
+
+    validate :content_or_attachment_must_be_present
+
+    private
+
+    def content_or_attachment_must_be_present
+        if content.blank? && !attachment.attached?
+        errors.add(:base, 'A mensagem deve ter um texto ou um anexo.')
+        end
+    end
 end
