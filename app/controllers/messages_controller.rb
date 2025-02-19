653a39c6b4617ecdbfaee1a6089df69fb5bffc25
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2025-02-19 11:22:07 -0300

    Correção dos anexos

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  include Rails.application.routes.url_helpers
+  
   before_action :authenticate_request
 
   def index
@@ -6,9 +8,9 @@ class MessagesController < ApplicationController
                       .where(receiver_id: [@current_user.id, params[:user_id]])
                       .order(created_at: :asc)
                       .page(params[:page]).per(10)
-
+  
     render json: {
-      messages: messages,
+      messages: messages.map { |msg| serialize_message(msg) },
       total_pages: messages.total_pages,
       current_page: messages.current_page
     }
@@ -17,13 +19,14 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.sender_id = @current_user.id
-
+  
     if @message.save
-      ActionCable.server.broadcast("messages_#{@message.receiver_id}", @message)
-
-      #ProcessMessageJob.perform_later(@message.id)
-
-      render json: @message, status: :created
+      serialized_message = serialize_message(@message)
+      ActionCable.server.broadcast("messages_#{@message.receiver_id}", serialized_message)
+  
+      ProcessMessageJob.perform_later(@message.id)
+  
+      render json: serialized_message, status: :created
     else
       render json: @message.errors, status: :unprocessable_entity
     end
@@ -34,4 +37,16 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:receiver_id, :content, :attachment)
   end
+
+  def serialize_message(message)
+    {
+      id: message.id,
+      sender_id: message.sender_id,
+      receiver_id: message.receiver_id,
+      content: message.content,
+      created_at: message.created_at,
+      attachment_url: message.attachment.attached? ? url_for(message.attachment) : nil,
+      attachment_content_type: message.attachment.attached? ? message.attachment.content_type : nil
+    }
+  end  
 end
